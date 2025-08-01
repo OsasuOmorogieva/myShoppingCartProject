fc31db2ae9bca917b0643f1a31c2a1139674decb
abimbola-ai <ojikutu.abimbola@gmail.com>
2025-08-01 12:19:57 -0400

    configured tailwindcss

--- frontend/shopping-cart/src/App.js
+++ frontend/shopping-cart/src/App.js
@@ -2,7 +2,11 @@ import logo from './logo.svg'
 import './App.css'
 
 function App() {
-  return <div className="App">Shopping Cart</div>
+  return (
+    <div className="App">
+      <h1 className="text-4xl font-bold text-blue-600"> Shopping Cart</h1>
+    </div>
+  )
 }
 
 export default App
